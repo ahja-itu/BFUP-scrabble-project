af2113964a8220247c98edf4eb967784e77aa63c
Andreas Wachs Hjalager <ahja@itu.dk>
2022-04-02 20:50:18 +0200

    Update NuGet password

--- Scripts/push_to_nuget.sh
+++ Scripts/push_to_nuget.sh
@@ -2,4 +2,4 @@
 
 
 cd ../ScrabbleBot
-dotnet nuget push bin/Release/EmmaGaddagBot.1.0.0.nupkg --source https://nuget.pkg.github.com/jesper-bengtson/index.json --api-key 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
+dotnet nuget push bin/Release/EmmaGaddagBot.1.0.0.nupkg --source https://nuget.pkg.github.com/jesper-bengtson/index.json --api-key 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
